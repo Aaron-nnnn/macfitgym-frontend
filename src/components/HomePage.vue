9106f6a47871696bce4ce325d8408dc9f0b8011a
aaron-nnnn <aaronnbarasabb@gmail.com>
2026-03-12 18:26:02 +0300

    homework

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,8 +5,8 @@
 <template>
     <v-container style="background-color:#FFFAF0" max-width="100%">
          <v-row>
-             <v-col md="4" class="d-flex align-centre justify-centre fill-height">
-                <v-img src="handsome-black-man-is-engaged-gym.jpg" height="500" cover></v-img>
+             <v-col md="4" class="d-flex align-centre justify-center fill-height">
+                <v-img src="woman-training-weightlifting-gym.jpg" height="500" cover></v-img>
             </v-col>
             <v-col md="8" class="text-centre">
                 <!-- <div class="text-title-large font-weight-light text-medium-emphasis mb-8">Our facility is designed to help you achieve your fitness goals in a supportive and motivating environment. Whether you are just starting your fitness journey or pushing to reach new limits, this is the right place for you.</div> -->
@@ -15,51 +15,106 @@
             </v-col>
          </v-row>
     </v-container>
-    <v-container style="background-color:#FFFAF0" class="mt-12">
+    <v-container style="background-color:#FFFAF0" class="text-center mt-12">
         <v-row>
                 <div class="text-display-medium mb-12">Programs offered.</div>
         </v-row>
         <v-row>
              <v-col md="4">
                 <v-card>
-                    <v-img src="dumbbells-row.jpg" cover></v-img>
+                    <v-icon color="#FFDDAA" icon="mdi-arm-flex" size="large" class="mt-8"></v-icon>
+                    <!-- <v-img src="lisa-marie-theck-kUGfnNMqLnA-unsplash.jpg" cover></v-img> -->
                     <v-card-title></v-card-title>
                     <v-card-text>Strength & Resistance Training - (Weightlifting / Free Weights – barbells, dumbbells ), (Bodybuilding Programs – muscle growth focus)</v-card-text>
                 </v-card>
             </v-col>
              <v-col md="4">
                 <v-card>
-                    <v-img src="woman-treadmill.jpg" cover></v-img>
+                    <v-icon color="#FFDDAA" icon="mdi-run" size="large" class="mt-8"></v-icon>
+                    <!-- <v-img src="woman-treadmill.jpg" cover></v-img> -->
                     <v-card-title></v-card-title>
                     <v-card-text>Cardio & Endurance - (Treadmill Running / Walking), (Stationary Bike / Spin Classes), (Rowing Machine Workouts), (High-Intensity Interval Training (HIIT))</v-card-text>
                 </v-card>
             </v-col>
              <v-col md="4">
                 <v-card>
-                    <v-img src="side-view-women-doing-yoga-together-beach.jpg" cover></v-img>
+                    <v-icon color="#FFDDAA" icon="mdi-yoga" size="large" class="mt-8"></v-icon>
+                    <!-- <v-img src="maryjoy-caballero-Um3NiahMQPY-unsplash.jpg" cover></v-img> -->
                     <v-card-title></v-card-title>
                     <v-card-text>Group Classes - (Yoga – flexibility, balance, and mindfulness), (Pilates – core strength & posture improvement), (Zumba / Dance Fitness – fun cardio sessions)</v-card-text>
                 </v-card>
             </v-col>
              <v-col md="4">
                 <v-card>
-                    <v-img src="quokkabottles-NN68U4SwSIs-unsplash.jpg" cover></v-img>
+                    <v-icon color="#FFDDAA" icon="mdi-handshake-outline" size="large" class="mt-8"></v-icon>
+                    <!-- <v-img src="lisa-marie-theck-Jmt0oJmJSn0-unsplash.jpg" cover></v-img> -->
                     <v-card-title></v-card-title>
                     <v-card-text>Personal Training - (One-on-one sessions tailored to individual goals), (Custom workout and nutrition plans), (Goal tracking & motivation support)</v-card-text>
                 </v-card>
             </v-col>
              <v-col md="4">
                 <v-card>
-                    <v-img src="boxed-water-is-better-JpWcneSvhDY-unsplash.jpg" cover></v-img>
+                    <v-icon color="#FFDDAA" icon="mdi-weight-lifter" size="large" class="mt-8"></v-icon>
+                    <!-- <v-img src="susosu-water-XTRtkl3YNeE-unsplash.jpg" cover></v-img> -->
                     <v-card-title></v-card-title>
                     <v-card-text>Specialty Programs - (Weight Loss Programs – fat-burning focused), (Strength & Conditioning – athletes or fitness enthusiasts)</v-card-text>
                 </v-card>
             </v-col>
              <v-col md="4">
                 <v-card>
-                    <v-img src="quokkabottles-NN68U4SwSIs-unsplash.jpg" cover></v-img>
+                    <v-icon color="#FFDDAA" icon="mdi-bed" size="large" class="mt-8"></v-icon>
+                    <!-- <v-img src="side-view-women-doing-yoga-together-beach.jpg" cover></v-img> -->
                     <v-card-title></v-card-title>
                     <v-card-text>Mind & Body Wellness - (Meditation & Relaxation Classes), (Breathing & Mobility Workshops), (Stretch & Recovery Sessions)</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+    <v-container  style="background-color:#FFFAF0" class="text-center mt-12">
+         <v-row>
+                <div class="text-display-medium mb-12">Meet our head Trainers.</div>
+        </v-row>
+         <v-row>
+             <v-col md="4">
+                <v-card>
+                    <v-img src="attractive-sports-girl-personal-trainer-middle-modern-gym-with-workout-plan-her-hands.jpg" cover></v-img> 
+                    <v-card-title></v-card-title>
+                    <v-card-text>Mary Cindy - Strength & Resistance Training Coach</v-card-text>
+                </v-card>
+            </v-col>
+             <v-col md="4">
+                <v-card>
+                    <v-img src="chris-lynch-e3JzXRSpGeg-unsplash.jpg" cover></v-img>
+                    <v-card-title></v-card-title>
+                    <v-card-text>Aaron Barasa - Cardio & Endurance Training Coach</v-card-text>
+                </v-card>
+            </v-col>
+             <v-col md="4">
+                <v-card>
+                    <v-img src="abdullah-ali-L2Nw-onZOX4-unsplash.jpg" cover></v-img> 
+                    <v-card-title></v-card-title>
+                    <v-card-text>Bill Kivoi - Group Classes Training Coach</v-card-text>
+                </v-card>
+            </v-col>
+             <v-col md="4">
+                <v-card>
+                    <v-img src="smiling-sporty-black-man-wearing-towel-around-neck.jpg" cover></v-img> 
+                    <v-card-title></v-card-title>
+                    <v-card-text>Fernendes Fernando - Personal Training Coach</v-card-text>
+                </v-card>
+            </v-col>
+             <v-col md="4">
+                <v-card>
+                    <v-img src="cheerful-young-sports-woman-with-arms-crossed.jpg" cover></v-img> 
+                    <v-card-title></v-card-title>
+                    <v-card-text>Brianah Briah - Specialty Programs Training Coach</v-card-text>
+                </v-card>
+            </v-col>
+             <v-col md="4">
+                <v-card>
+                    <v-img src="portrait-smiling-afro-american-sports-man-with-arms-folded-looking-camera.jpg" cover></v-img> 
+                    <v-card-title></v-card-title>
+                    <v-card-text>Allah Allan - Mind & Body Wellness Training Coach</v-card-text>
                 </v-card>
             </v-col>
         </v-row>
